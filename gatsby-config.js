27c36903588c8e74dcde4fbdd4ce899c869e6f6d
Matt Watson <matt@missingwires.com>
2020-01-08 17:58:41 +1100

    Initial gatsby checkin

---
+++ gatsby-config.js
@@ -0,0 +1,51 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  siteMetadata: {
+    title: `Missing Wires`,
+    author: `Matt Watson`,
+    description: `Matt's Blog.`,
+    siteUrl: `https://missingwires.com/`
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 800,
+            },
+          },
+          `gatsby-remark-copy-linked-files`,
+        ],
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
+  }
--- styles-ec66d17afd9a641ad963.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{"0sYV":function(n,w,o){}}]);
-//# sourceMappingURL=styles-ec66d17afd9a641ad963.js.map
