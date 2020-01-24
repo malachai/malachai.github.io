d41df1b30eeffcd6259c4adc99e588426fc8d18c
Matt Watson <matthew.watson@mullenloweprofero.com>
2020-01-24 11:41:53 +1100

    Removed imagesharp due to it interferring with preview functionality

--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -39,13 +39,6 @@ query BlogPostQuery($uid: String) {
             }
           }
           image
-          imageSharp {
-            childImageSharp {
-              fluid(maxWidth: 400, maxHeight: 250) {
-                ...GatsbyImageSharpFluid
-              }
-            }
-          }
         }
       }
     }
